0e4369a9c27f6a4dbe78477ef7ef4ede2622db31
Anton Malov <maloanto@yandex.ru>
2024-04-24 23:02:52 +0300

    add lombok, pojo and test

--- src/test/java/tests/juinit5/API/fakestoreapi/UsersTests.java
+++ src/test/java/tests/juinit5/API/fakestoreapi/UsersTests.java
@@ -1,12 +1,15 @@
 package tests.juinit5.API.fakestoreapi;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
+import models.fakeapiuser.Address;
+import models.fakeapiuser.Geolocation;
+import models.fakeapiuser.Name;
+import models.fakeapiuser.UserRoot;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -24,7 +27,7 @@ public class UsersTests {
 
     @Test
     public void getSingleUserTest() {
-        int userId = 2;
+        Integer userId = (Integer) 2;
         given().pathParam("userId", userId)
                 .get("https://fakestoreapi.com/users/{userId}")
                 .then()
@@ -37,7 +40,7 @@ public class UsersTests {
     @Test
     public void getAllUsersWithLimitTest() {
         int limitSize = 3;
-        given().queryParam("limit", limitSize)
+        given().queryParam("limit", Collections.singleton(limitSize))
                 .get("https://fakestoreapi.com/users")
                 .then().log().all()
                 .statusCode(200)
@@ -65,5 +68,37 @@ public class UsersTests {
 
         assertNotEquals(sortedListIds, notSortedListIds);
         assertEquals(sortedByCode, sortedListIds);
+    }
+
+    @Test
+    public void addNewUserTest() {
+        Name name = new Name("Anton", "Malov");
+        Geolocation geolocation = new Geolocation("-45.4532", "34.2345");
+        Address address = Address.builder()
+                .geolocation(geolocation)
+                .city("Moscow")
+                .street("street")
+                .number(4556)
+                .zipcode("12345-2365")
+                .build();
+
+        UserRoot bodyRequest = UserRoot.builder()
+                .address(address)
+                .email("some@email")
+                .username("ant")
+                .password("somePass")
+                .name(name)
+                .phone("79898989898")
+                .build();
+
+        given().body(bodyRequest)
+                .post("https://fakestoreapi.com/users")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", notNullValue());
+
+
+
+
     }
 }
