4a9b3ecc1a2ee07d8dd9f5fd68c63c9ce04ee508
Anton Malov <maloanto@yandex.ru>
2024-04-24 21:46:16 +0300

    add test sorted

--- src/test/java/tests/juinit5/API/fakestoreapi/UsersTests.java
+++ src/test/java/tests/juinit5/API/fakestoreapi/UsersTests.java
@@ -1,9 +1,17 @@
 package tests.juinit5.API.fakestoreapi;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UsersTests {
     @Test
@@ -34,5 +42,28 @@ public class UsersTests {
                 .then().log().all()
                 .statusCode(200)
                 .body("", hasSize(limitSize));
+    }
+
+    @Test
+    public void getAllUsersSortByDescTest() {
+        String sortType = "desc";
+        Response sortedResponse = given().queryParam("sort", sortType)
+                .get("https://fakestoreapi.com/users")
+                .then().log().all()
+                .statusCode(200)
+                .extract().response();
+
+        Response notSortedResponse = given()
+                .get("https://fakestoreapi.com/users")
+                .then().log().all()
+                .extract().response();
+
+        List<Integer> sortedListIds = sortedResponse.jsonPath().getList("id");
+        List<Integer> notSortedListIds = notSortedResponse.jsonPath().getList("id");
+
+        List<Integer> sortedByCode = notSortedListIds.stream().sorted(Comparator.reverseOrder()).toList();
+
+        assertNotEquals(sortedListIds, notSortedListIds);
+        assertEquals(sortedByCode, sortedListIds);
     }
 }
