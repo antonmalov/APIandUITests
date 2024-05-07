39cd00211aee0ca34a7b2706f4f415774f7921ec
Anton Malov <maloanto@yandex.ru>
2024-05-07 21:53:06 +0300

    add test for simple form test

--- src/test/java/ui/SeleniumTests.java
+++ src/test/java/ui/SeleniumTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -33,5 +35,47 @@ public class SeleniumTests {
         String actualTitle = driver.getTitle();
         String expectedTitle = "Oleh Pendrak - YouTube";
         Assertions.assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Test
+    public void simpleFormTest() {
+        String expectedName = "Tomas Anderson";
+        String expectedEmail = "tomas@matrix.ru";
+        String expectedCurrentAddress = "USA LA";
+        String expectedPermanentAddress = "USA Miami";
+
+        driver.get("http://85.192.34.140:8081/");
+        WebElement elementCard = driver.findElement(By.xpath("//div[@class='card-body']//h5[text()='Elements']"));
+        elementCard.click();
+
+        WebElement elementTextBox = driver.findElement(By.xpath("//span[text()='Text Box']"));
+        elementTextBox.click();
+
+        WebElement userName = driver.findElement(By.id("userName"));
+        WebElement userEmail = driver.findElement(By.id("userEmail"));
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        WebElement btnSubmit = driver.findElement(By.id("submit"));
+
+        userName.sendKeys(expectedName);
+        userEmail.sendKeys(expectedEmail);
+        currentAddress.sendKeys(expectedCurrentAddress);
+        permanentAddress.sendKeys(expectedPermanentAddress);
+        btnSubmit.click();
+
+        WebElement nameNew = driver.findElement(By.id("name"));
+        WebElement emailNew = driver.findElement(By.id("email"));
+        WebElement currentAddressNew = driver.findElement(By.xpath("//div[@id='output']//p[@id='currentAddress']"));
+        WebElement permanentAddressNew = driver.findElement(By.xpath("//div[@id='output']//p[@id='permanentAddress']"));
+
+        String actualName = nameNew.getText();
+        String actualEmail = emailNew.getText();
+        String actualCurrentAddress = currentAddressNew.getText();
+        String actualPermanentAddress = permanentAddressNew.getText();
+
+        Assertions.assertTrue(actualName.contains(expectedName));
+        Assertions.assertTrue(actualEmail.contains(expectedEmail));
+        Assertions.assertTrue(actualCurrentAddress.contains(expectedCurrentAddress));
+        Assertions.assertTrue(actualPermanentAddress.contains(expectedPermanentAddress));
     }
 }
